b80241d476c0ce74d60444c3126a5d2b55fd8d29
Chudnovskiy.O.W520 <oleg.chudnovskiy@gmail.com>
2022-03-03 20:26:09 +0200

    add FragmentOne and FragmentTwo layouts

---
+++ app/src/main/java/com/chudnovskiy/devcolibri_l26_transferdatabetweenfragments/MainActivity.java
@@ -0,0 +1,14 @@
+package com.chudnovskiy.devcolibri_l26_transferdatabetweenfragments;
+
+import android.support.v4.app.FragmentActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends FragmentActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/com/chudnovskiy/devcolibri_l26_transferdatabetweenfragments/fragment/TwoFragment.java
@@ -0,0 +1,19 @@
+package com.chudnovskiy.devcolibri_l26_transferdatabetweenfragments.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.chudnovskiy.devcolibri_l26_transferdatabetweenfragments.R;
+
+public class TwoFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.second_fragment_layout, container, false);
+    }
+}
