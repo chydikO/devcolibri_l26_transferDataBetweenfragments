d15010dd1cc9255801e0f0d1409cba0239100449
Chudnovskiy.O.W520 <oleg.chudnovskiy@gmail.com>
2022-03-04 17:47:14 +0200

    l26_transferdatabetweenfragments 6-56

--- app/src/main/java/com/chudnovskiy/devcolibri_l26_transferdatabetweenfragments/MainActivity.java
+++ app/src/main/java/com/chudnovskiy/devcolibri_l26_transferdatabetweenfragments/MainActivity.java
@@ -1,14 +1,29 @@
 package com.chudnovskiy.devcolibri_l26_transferdatabetweenfragments;
 
 import android.support.v4.app.FragmentActivity;
-import android.support.v7.app.AppCompatActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.os.Bundle;
 
+import com.chudnovskiy.devcolibri_l26_transferdatabetweenfragments.fragment.TwoFragment;
+
 public class MainActivity extends FragmentActivity {
+
+    private FragmentManager fragmentManager;
+    private FragmentTransaction fragmentTransaction;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        fragmentManager = getSupportFragmentManager();
+        initFragmentLast();
+    }
+
+    private void initFragmentLast() {
+        fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.add(R.id.container, new TwoFragment());
+        fragmentTransaction.commit();
     }
 }
